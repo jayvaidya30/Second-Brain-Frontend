cd7db2bcd85ca73782c26599442597a1e99b1283
Jay <jayvaidya3009@gmail.com>
2025-12-03 22:07:43 +0530

    Add button and card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Button } from "./components/Button";
+import { PlusIcon } from "./Icons/PlusIcon";
+import { ShareIcon } from "./Icons/ShareIcon";
+import { Card } from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="p-4">
+      <div className="flex justify-end gap-4">
+        <Button
+          variant="primary"
+          text="Add content"
+          startIcon={<PlusIcon></PlusIcon>}
+        ></Button>
+        <Button
+          variant="secondary"
+          text="Share"
+          startIcon={<ShareIcon></ShareIcon>}
+        ></Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex gap-4">
+        <Card
+          type="twitter"
+          link="https://x.com.com/jayvaidya30/status/1865407035594232093?s=20"
+          title="Twitter link test"
+        ></Card>
+        <Card
+          type="youtube"
+          link="https://www.youtube.com/watch?v=njX2bu-_Vw4"
+          title="First video test"
+        ></Card>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import type { ReactElement } from "react";
+
+interface ButtonProps {
+  variant: "primary" | "secondary";
+  size?: "sm" | "md" | "lg";
+  text: string;
+  startIcon?: ReactElement;
+  endIcon?: ReactElement;
+  onClick?: () => void;
+}
+
+const variantStyles = {
+  primary: "bg-purple-600 text-white",
+  secondary: "bg-purple-200 text-purple-600",
+};
+
+const defaultStyles = "px-4 py-2 rounded-md flex items-center";
+
+export const Button = (props: ButtonProps) => {
+  return (
+    <button className={variantStyles[props.variant] + " " + defaultStyles}>
+      <div className="pr-2">{props.startIcon}</div>
+      {props.text}
+    </button>
+  );
+};
