549dae0bc1bc0bdb46df2f6c0079f34c768d9f60
Jay <jayvaidya3009@gmail.com>
2025-12-07 21:05:53 +0530

    Add fetching for user contents

--- src/components/CreateContentModal.tsx
+++ src/components/CreateContentModal.tsx
@@ -1,7 +1,9 @@
+import axios from "axios";
 import { useRef, useState } from "react";
 import { CrossIcon } from "../Icons/CrossIcon";
 import { Button } from "./Button";
 import { Input } from "./Input";
+import { BACKEND_URL } from "../config";
 
 enum ContentType {
   Youtube = "youtube",
@@ -14,17 +16,29 @@ export function CreateContentModal({ open, onClose }) {
 
   const titleRef = useRef<HTMLInputElement>();
   const linkRef = useRef<HTMLInputElement>();
+  const [type, setType] = useState(ContentType.Youtube);
 
   async function addContent() {
     const title = titleRef.current?.value;
     const link = linkRef.current?.value;
-    const [type, setType] = useState(ContentType.Youtube);
+
+    await axios.post(`${BACKEND_URL}/api/v1/content`, {
+      link,
+      title,
+      type
+    }, {
+      headers: {
+        "Authorization": localStorage.getItem("token")
+      }
+    })
+
+    onClose();
   }
 
   return (
     <div>
       {open && (
-        <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-60 flex justify-center items-center">
+        <div className="w-screen h-screen bg-slate-500 fixed top-0 left-0 opacity-80 flex justify-center items-center">
           <div className="bg-white p-4 rounded">
             <div className="flex justify-end">
               <div onClick={onClose} className="cursor-pointer">
@@ -35,6 +49,29 @@ export function CreateContentModal({ open, onClose }) {
             <div>
               <Input reference={titleRef} placeholder={"Title"} />
               <Input reference={linkRef} placeholder={"Link"} />
+            </div>
+            <div>
+            <h1>Type</h1>
+              <div className="flex gap-2 justify-center p-2">
+                <Button
+                  text="Youtube"
+                  variant={
+                    type === ContentType.Youtube ? "primary" : "secondary"
+                  }
+                  onClick={() => {
+                    setType(ContentType.Youtube);
+                  }}
+                ></Button>
+                <Button
+                  text="Twitter"
+                  variant={
+                    type === ContentType.Twitter ? "primary" : "secondary"
+                  }
+                  onClick={() => {
+                    setType(ContentType.Twitter);
+                  }}
+                ></Button>
+              </div>
             </div>
 
             <div className="flex justify-center">
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,21 +1,27 @@
-import { useState } from "react";
+import { useEffect, useEffectEvent, useState } from "react";
 import { Button } from "../components/Button";
 import { PlusIcon } from "../Icons/PlusIcon";
 import { ShareIcon } from "../Icons/ShareIcon";
 import { Card } from "../components/Card";
 import { CreateContentModal } from "../components/CreateContentModal";
 import { SideBar } from "../components/Sidebar";
+import { useContent } from "../hooks/useContent";
+import axios from "axios";
+import { BACKEND_URL } from "../config";
 
 export function Dashboard() {
   const [modalOpen, setModelOpen] = useState(false);
+  const { contents, refresh } = useContent();
+
+  // when the modal closes the contents of the user will refresh
+  useEffect(() => {
+    refresh();
+  }, [modalOpen]);
 
   return (
     <div>
-      
-    <SideBar />
+      <SideBar />
 
-
-    {/* main content */}
       <div className="p-4 ml-72 min-h-screen bg-gray-300">
         <CreateContentModal
           open={modalOpen}
@@ -32,22 +38,31 @@ export function Dashboard() {
             onClick={() => setModelOpen(true)}
           ></Button>
           <Button
+            onClick={async () => {
+              const response = await axios.post(
+                `${BACKEND_URL}/api/v1/brain/share`,
+                {
+                  share: true,
+                }, {
+                  headers: {
+                    "Authorization" : localStorage.getItem("token")
+                  }
+                }
+              );
+
+              const shareUrl = `https://localhost:5173/share/${response.data.hash}`;
+              navigator.clipboard.writeText(shareUrl);
+              alert("Link copied to clipboard!");
+            }}
             variant="secondary"
             text="Share"
             startIcon={<ShareIcon></ShareIcon>}
           ></Button>
         </div>
-        <div className="flex gap-4">
-          <Card
-            type="twitter"
-            link="https://x.com.com/jayvaidya30/status/1865407035594232093?s=20"
-            title="Twitter link test"
-          ></Card>
-          <Card
-            type="youtube"
-            link="https://www.youtube.com/watch?v=njX2bu-_Vw4"
-            title="First video test"
-          ></Card>
+        <div className="flex gap-4 flex-wrap">
+          {contents.map(({ type, link, title }) => (
+            <Card type={type} link={link} title={title} />
+          ))}
         </div>
       </div>
     </div>
